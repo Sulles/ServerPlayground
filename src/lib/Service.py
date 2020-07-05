42a215ab3e190a08e54991c2311e29c71ee78cae
Sulles <suleymanbarthesukhera@gmail.com>
2020-07-05 18:59:12 -0400

    added lib, updated gitignore

---
+++ src/lib/Service.py
@@ -0,0 +1,190 @@
+"""
+=============== SERVICES ===============
+This file houses the global Service class definitions that all components can interact with.
+Two subclasses, InsecureService and SecureService are differentiations of the base Service class meant to hide/provide
+    security-dependent APIs to users.
+
+Both objects, InsecureService and SecureService are initialized in src.__init__ and accessible to all components,
+    so be careful when registering functions/methods to specific services
+
+
+=== Service Architecture ===
+Services work as a data-forwarding system where data in the form of a dictionary is passed to a callback function
+__WARNING__: call_back functions should be mutex protected to prevent multiple simultaneous access requests by different
+    users.
+
+Data passed to a Service object must be a Dictionary and have the following keys: request, data.
+    Optional keys: client_id, command_id
+        - or any other field necessary to process the service request successfully
+
+The data dictionary handled by the service will be returned with an additional 'response' field. The 'response' field
+    is what should be communicated back to the client, data is kept within the dictionary as a means of tracking
+    command/response handling.
+Chaining services is feasible and the data dictionary from a previous service call should be appended to a List called
+    'previous_services'.
+
+:: Example of service definition:
+    def my_service(self, data: dict = None):
+        with self.mutex_lock:
+            # handle data
+            return response
+
+:: Example of service registration:
+    INSECURE_SERVICE.register_service('my_service_name', self.my_service)
+
+"""
+
+from src.server import MAX_QUEUE_SIZE
+from .util import LogWorthy, lockable
+from .. import Queue, RLock
+
+
+class Service(LogWorthy):
+    def __init__(self, name, file_name=None):
+        """ Service object is a glorified Queue that properly triages requests based on registered endpoints
+        :param name: name of service object
+        """
+        LogWorthy.__init__(self, file_name)
+        self.name = name
+        self.file_name = file_name
+        self.lock = RLock()
+        self.services = dict()
+        self.oneshots = dict()
+        self.queue = Queue(maxsize=MAX_QUEUE_SIZE)
+        self.register_service('echo', self.echo)
+        self.register_service('help', self.get_help)
+
+    @lockable
+    def register_service(self, service_name: str, call_back: classmethod):
+        self.log(f'Registering endpoint: {service_name}')
+        self.services[service_name] = call_back
+
+    @lockable
+    def unregister_service(self, service_name: str):
+        self.log(f'Unregistering endpoint: {service_name}')
+        del self.services[service_name]
+
+    # @lockable
+    # def create_oneshot(self, oneshot_name, call_back):
+    #     self.log(f'Creating oneshot: {oneshot_name}')
+    #     self.oneshots[oneshot_name] = call_back
+    #
+    # @lockable
+    # def delete_oneshot(self, oneshot_name):
+    #     self.log(f'Deleting oneshot: {oneshot_name}')
+    #     del self.oneshots[oneshot_name]
+
+    """ === HANDLER === """
+
+    @lockable
+    def _handle(self, data: dict = None):
+        self.debug('Got into handler...')
+        if 'request' not in data.keys() or 'data' not in data.keys():
+            data['response'] = 'ERROR: Service request dictionary must have "request" and "data" fields'
+            return data
+
+        self.log(f'Got service request: {data["request"]} with data: {data["data"]}')
+        if data['request'] in self.services.keys():
+            response = self.services[data['request']](data)
+            self.debug(f'Got response:\n{response}')
+            return response
+        else:
+            self.log('Got unknown request, returning help')
+            return self.get_help(data)
+        # else:
+        #     self.log(f'Got service request: {data}')
+        #     if data in self.services.keys():
+        #         response = self.services[data]()
+        #         self.debug(f'Got response:\n{response}')
+        #         return response
+        #     else:
+        #         self.log(f'Got unknown data {data}, returning help')
+        #         return self.get_help()
+
+    """ === GETTERS === """
+
+    @lockable
+    def get_services(self):
+        return self.services.keys()
+
+    # @lockable
+    # def get_oneshots(self):
+    #     return self.oneshots.keys()
+
+    """ === DEFAULT SERVICES === """
+
+    @staticmethod
+    def echo(data: dict = None):
+        data['response'] = data['data']
+        return data
+
+    def get_help(self, data: dict = None):
+        help_data = f'=== {self.name} Help ===\n'
+        if data is not None and 'client_id' in data.keys():
+            help_data += f'Your connection is at: {data["client_id"]}\n'
+        help_data += 'Available services:\n'
+        for _ in self.services.keys():
+            help_data += f'\t{_}\n'
+        help_data += f'\texit\n'    # Exit handled by ClientConnection object
+        help_data += '\n'
+        help_data += 'Commands are space delimited\n'
+        help_data += 'I.e. "echo hello world", "echo" is the service name,\n' \
+                     'and "hello world" is data passed to the service'
+        data['response'] = help_data
+        return data
+
+    """ === MISC === """
+
+    def log(self, log):
+        self._log(f'[{self.name}] {log}')
+
+    def debug(self, log):
+        self._debug(f'[{self.name}] {log}')
+
+
+class SecureService(Service):
+    """ Secure Service object that houses all secure services """
+
+    def __init__(self, file_name: str = None):
+        self.name = "SecureService"
+        Service.__init__(self, self.name, file_name)
+
+    def handle(self, data: dict = None):
+        if data is None or type(data) is not dict:
+            self.log(f'Invalid service data, got: {data} of type: {type(data)}')
+            return
+        self.log(f'Handling {data}')
+        return self._handle(data)
+        # if ' ' in data:
+        #     request_type, request_data = data.split(' ', 1)
+        #     if request_type not in self.services.keys():
+        #         self.log(f'Got invalid insecure service request: "{request_type}", returning "invalid request"')
+        #         return 'Error: Invalid request type, try "help"'
+        #     else:
+        #         return self._handle(data)
+        # else:
+        #     return self._handle(data)
+
+
+class InsecureService(Service):
+    """ Insecure Service object that houses all insecure services """
+
+    def __init__(self, file_name=None):
+        self.name = "InsecureService"
+        Service.__init__(self, self.name, file_name)
+
+    def handle(self, data):
+        if data is None or type(data) is not dict:
+            self.log(f'Invalid service data, got: {data} of type: {type(data)}')
+            return
+        self.log(f'Handling {data}')
+        return self._handle(data)
+        # if ' ' in data:
+        #     request_type, request_data = data.split(' ', 1)
+        #     if request_type not in self.services.keys():
+        #         self.log(f'Got invalid insecure service request: "{request_type}", returning "invalid request"')
+        #         return 'Error: Invalid request type, try "help"'
+        #     else:
+        #         return self._handle(data)
+        # else:
+        #     return self._handle(data)
---
+++ src/lib/util.py
@@ -0,0 +1,66 @@
+"""
+Main util file with miscellaneous common variables, functions, and classes
+"""
+
+from datetime import datetime
+
+MAX_QUEUE_SIZE = 5
+
+
+def lockable(func):
+    """ Thread/process-safe locking decorator for each instance of an object """
+
+    def wrapper(this, *args, **kwargs):
+        """
+        Wrapper method takes object and all args/kwargs and forwards to the func(tion) that is being decorated
+        :param this: Object reference (self), object MUST have a .lock attribute (ideally RLock) and .debug method
+        :param args: arbitrary arguments passed to "func"
+        :param kwargs: arbitrary key-word arguments passed to "func"
+        :return: whatever "func(*args, **kwargs)" returns normally
+        """
+        this.debug(f'{func} using lock')
+        with this.lock:
+            return func(this, *args, **kwargs)
+
+    return wrapper
+
+
+class LogWorthy(object):
+    """ Basic Logging object """
+
+    def __init__(self, file_name=None):
+        self.file_name = file_name
+        self.log_file = None
+        if file_name is not None:
+            self.log_file = open(self.file_name, 'a')
+
+    def _log(self, log):
+        improved_log = f'[{datetime.now()}] {log}'
+        print(improved_log)
+        self._debug(log)
+
+    def _debug(self, log):
+        if self.file_name is not None:
+            log = f'[{datetime.now()}] {log}'
+            with self.log_file:
+                self.log_file.writelines('\n' + log)
+
+
+def kill_thread(logger, thread):
+    tries = 0
+    try:
+        while thread.is_alive():
+            tries += 1
+            thread.join(timeout=1)
+            if tries > 3:
+                logger('Killing thread ungracefully!')
+                thread._stop()
+                return
+        logger('Thread closed gracefully')
+    except AttributeError as e:
+        logger(f'Thread is already closed?\n{e}')
+    except RuntimeError as e:
+        logger(f'Failed to close with error: {e}')
+    except Exception as e:
+        logger(f'WUT?')
+        raise e
